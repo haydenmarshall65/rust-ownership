1acbab65e095041530797ec857ed93dbdd443e8a
haydenmarshall65 <hmarshall@altorntl.com>
2024-08-13 12:00:13 -0500

    finish ownership learning

--- src/main.rs
+++ src/main.rs
@@ -27,4 +27,30 @@ fn main() {
     let x = 5;
     let y = x;
     println!("{} {}, this is not moved, because integers have a fixed size on the stack.", x, y);
+
+    let new_str = String::from("hello");
+    takes_ownership(new_str);
+    let new_x = 5;
+    makes_copy(new_x);
+    let str_5 = gives_ownership();
+    let str_6 = takes_and_gives_ownership(str_5);
+    println!("{}, this was moved and dropped, but has returned a new string to the caller", str_6);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}, you can't use this anymore", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn takes_and_gives_ownership(some_string: String) -> String {
+    let some_string = String::from(some_string);
+    some_string
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
 }
